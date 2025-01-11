324b9d9f2a43ecd6cbd9894cebeb91622ac84e40
NanoShadow <157168429+NanoShadow256@users.noreply.github.com>
2025-01-11 22:31:45 +0100

    maze is not randomized for now (+ a lot of stuff)

--- main.js
+++ main.js
@@ -8,14 +8,40 @@ const ctx = document.getElementById('canv').getContext('2d');
 
 let min_dir = 0;
 
+
+const U = 'w_up';
+const R = 'w_right';
+const D = 'w_down';
+const L = 'w_left';
+const C = 'tp_ci';
+const S = 'tp_sq';
+
+const theme = {
+	'game_bg': '',
+	'circlee': '#00c',
+	'circlee_border': '#56f',
+	'squaree': '#e90',
+	'squaree_border':'#940',
+	'border_w': 1,
+	'wall': '#72b',
+	'wall_border': '#308',
+	'circlee_tp': ['#025', '#14a', '#07e'], // outside to inside
+	'squaree_tp': ['#520', '#950', '#d60'],
+	'slow': ['#f2a', '#fff'],
+	'speedy': ['#9f96', '#4f4f'], // start (lower opacity) to end
+	'speedy_arr': '#085'
+};
+
 const circlee = {
 	direction: [0, 0],
 	x: 100,
 	y: 100,
 	size: 7,
 	speed: 10,
+	base_speed: 10,
 	last_x: 100,
-	last_y: 100
+	last_y: 100,
+	do_not_tp: false
 };
 
 const squaree = {
@@ -24,8 +50,10 @@ const squaree = {
 	y: 100,
 	size: 7,
 	speed: 5,
+	base_speed: 5,
 	last_x: 100,
-	last_y: 100
+	last_y: 100,
+	do_not_tp: false
 };
 
 function gridConvert(coordinates, fromG = true, center = false) {
@@ -41,6 +69,14 @@ function gridConvert(coordinates, fromG = true, center = false) {
 		return [(coordinates[0] - coordinates[0] % (canv_w / cols)) / (canv_w / cols),
 		(coordinates[1] - coordinates[1] % (canv_h / rows)) / (canv_h / rows)];
 	}
+}
+
+function spark4(x, y, w, h, a) {
+	ctx.moveTo(x, y - h / 2);
+	ctx.quadraticCurveTo(x + w * a,	y - h * a, x + w / 2, y);
+	ctx.quadraticCurveTo(x + w * a,	y + h * a, x, y + h / 2);
+	ctx.quadraticCurveTo(x - w * a,	y + h * a, x - w / 2, y);
+	ctx.quadraticCurveTo(x - w * a,	y - h * a, x, y - h / 2);
 }
 
 const game = {
@@ -142,36 +178,176 @@ const game = {
 		wall_h = canv_h / rows;
 	},
 	drawMaze: () => {
-		ctx.fillStyle = '#a0c';
 		for (let n = 0; n < rows; n++) {
 			for (let m = 0; m < cols; m++) {
-				if (maze[n][m]) {
+				if (maze[n][m] == 1) {
 					// ctx.fillRect(m * wall_w, n * wall_h, wall_w, wall_h);
+					// ctx.fillStyle = theme['wall'];
+					// ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), Math.ceil(wall_w), Math.ceil(wall_h));
+					ctx.fillStyle = theme['wall'];
 					ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), Math.ceil(wall_w), Math.ceil(wall_h));
+					ctx.fillStyle = theme['wall_border'];
+					if (maze[n][m + 1] != 1) {
+						ctx.fillRect(Math.ceil(m * wall_w + wall_w) - 2, Math.floor(n * wall_h), 2, Math.ceil(wall_h));
+					}
+					if (maze[n][m - 1] != 1) {
+						ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), 2, Math.ceil(wall_h));
+					}
+					if (!maze[n + 1] || maze[n + 1][m] != 1) {
+						ctx.fillRect(Math.floor(m * wall_w), Math.ceil(n * wall_h + wall_h) - 2, Math.ceil(wall_w), 2);
+					}
+					if (!maze[n - 1] || maze[n - 1][m] != 1) {
+						ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), Math.ceil(wall_w), 2);
+					}
+					if (maze[n + 1]) {
+						if (maze[n + 1][m + 1] != 1) {
+							ctx.fillRect(Math.ceil(m * wall_w + wall_w) - 2, Math.ceil(n * wall_h + wall_h) - 2, 2, 2);
+						}
+						if (maze[n + 1][m - 1] != 1) {
+							ctx.fillRect(Math.floor(m * wall_w), Math.ceil(n * wall_h + wall_h) - 2, 2, 2)
+						}
+					}
+					if (maze[n - 1]) {
+						if (maze[n - 1][m + 1] != 1) {
+							ctx.fillRect(Math.ceil(m * wall_w + wall_w) - 2, Math.floor(n * wall_h), 2, 2);
+						}
+						if (maze[n - 1][m - 1] != 1) {
+							ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), 2, 2)
+						}
+					}
+				} else if (maze[n][m] == 2) {
+					const grad1 = ctx.createRadialGradient(Math.floor(m * wall_w + wall_w / 2), Math.floor(n * wall_h + wall_h / 2), 0, Math.floor(m * wall_w + wall_w / 2), Math.floor(n * wall_h + wall_h / 2), Math.max(wall_w, wall_h) / 2);
+					grad1.addColorStop(0, theme['slow'][1]);
+					grad1.addColorStop(1, theme['slow'][0]);
+					ctx.fillStyle = grad1;
+					ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), Math.ceil(wall_w), Math.ceil(wall_h));
+				} else if (maze[n][m] == 3) {
+					ctx.fillStyle = '#000';
+					ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), Math.ceil(wall_w), Math.ceil(wall_h));
+					let xx = 2;
+					let xx2 = 10;
+					ctx.fillStyle = '#666';
+					ctx.beginPath();
+					spark4(m * wall_w + wall_w / 2, n * wall_h + wall_h / 2, wall_w - 2, wall_h - 2, 0.1);
+					ctx.fill();
+					ctx.fillStyle = '#fff';
+					ctx.beginPath();
+					spark4(m * wall_w + wall_w / 2, n * wall_h + wall_h / 2, wall_w - wall_w / 4, wall_h - wall_h / 4, 0);
+					ctx.fill();
+				} else if (maze[n][m] == C) {
+					ctx.fillStyle = theme['circlee_tp'][0];
+					ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), Math.ceil(wall_w), Math.ceil(wall_h));
+					ctx.fillStyle = theme['circlee_tp'][1]; ctx.beginPath();
+					ctx.ellipse(m * wall_w + wall_w / 2, n * wall_h + wall_h / 2, wall_w / 2, wall_h / 2, 0, 0, 2 * Math.PI); ctx.fill();
+					ctx.fillStyle = theme['circlee_tp'][2]; ctx.beginPath();
+					ctx.arc(m * wall_w + wall_w / 2, n * wall_h + wall_h / 2, Math.min(wall_w, wall_h) / 3, 0, 2 * Math.PI); ctx.fill();
+				} else if (maze[n][m] == S) {
+					ctx.fillStyle = theme['squaree_tp'][0];
+					ctx.fillRect(Math.floor(m * wall_w), Math.floor(n * wall_h), Math.ceil(wall_w), Math.ceil(wall_h));					
+					ctx.fillStyle = theme['squaree_tp'][1]; ctx.beginPath();
+					ctx.moveTo(m * wall_w + wall_w / 2, n * wall_h);
+					ctx.lineTo(m * wall_w + wall_w, n * wall_h + wall_h / 2);
+					ctx.lineTo(m * wall_w + wall_w / 2, n * wall_h + wall_h);
+					ctx.lineTo(m * wall_w, n * wall_h + wall_h / 2);
+					ctx.lineTo(m * wall_w + wall_w / 2, n * wall_h);
+					ctx.fill();
+					ctx.fillStyle = theme['squaree_tp'][2];
+					ctx.fillRect(m * wall_w + wall_w / 4, n * wall_h + wall_h / 4, wall_w - (wall_w / 2), wall_h - (wall_h / 2));
+				} else if (maze[n][m] == L) {
+					const grad1 = ctx.createLinearGradient(Math.floor(m * wall_w), 0, Math.ceil(m * wall_w + wall_w), 0);
+					grad1.addColorStop(0, theme['speedy'][1]);
+					grad1.addColorStop(1, theme['speedy'][0]);
+					ctx.fillStyle = grad1;
+					ctx.fillRect(Math.ceil(m * wall_w), Math.ceil(n * wall_h), Math.floor(wall_w), Math.floor(wall_h));
+					ctx.fillStyle = theme['speedy_arr'];
+					ctx.beginPath();
+					ctx.moveTo(m * wall_w + wall_w / 10, n * wall_h + wall_h / 2);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 4, n * wall_h + wall_h - wall_h / 10);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 4, n * wall_h + wall_h - wall_h / 5);
+					ctx.lineTo(m * wall_w + wall_w / 2.5, n * wall_h + wall_h / 2);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 4, n * wall_h + wall_h / 5);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 4, n * wall_h + wall_h / 10);
+					ctx.closePath();
+					ctx.fill();
+				} else if (maze[n][m] == R) {
+					const grad1 = ctx.createLinearGradient(Math.floor(m * wall_w), 0, Math.ceil(m * wall_w + wall_w), 0);
+					grad1.addColorStop(0, theme['speedy'][0]);
+					grad1.addColorStop(1, theme['speedy'][1]);
+					ctx.fillStyle = grad1;
+					ctx.fillRect(Math.ceil(m * wall_w), Math.ceil(n * wall_h), Math.floor(wall_w), Math.floor(wall_h));
+					ctx.fillStyle = theme['speedy_arr'];
+					ctx.beginPath();
+					ctx.moveTo(m * wall_w + wall_w - wall_w / 10, n * wall_h + wall_h / 2);
+					ctx.lineTo(m * wall_w + wall_w / 4, n * wall_h + wall_h - wall_h / 10);
+					ctx.lineTo(m * wall_w + wall_w / 4, n * wall_h + wall_h - wall_h / 5);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 2.5, n * wall_h + wall_h / 2);
+					ctx.lineTo(m * wall_w + wall_w / 4, n * wall_h + wall_h / 5);
+					ctx.lineTo(m * wall_w + wall_w / 4, n * wall_h + wall_h / 10);
+					ctx.closePath();
+					ctx.fill();
+				} else if (maze[n][m] == U) {
+					const grad1 = ctx.createLinearGradient(0, Math.floor(n * wall_h), 0, Math.ceil(n * wall_h + wall_h));
+					grad1.addColorStop(0, theme['speedy'][1]);
+					grad1.addColorStop(1, theme['speedy'][0]);
+					ctx.fillStyle = grad1;
+					ctx.fillRect(Math.ceil(m * wall_w), Math.ceil(n * wall_h), Math.floor(wall_w), Math.floor(wall_h));
+					ctx.fillStyle = theme['speedy_arr'];
+					ctx.beginPath();
+					ctx.moveTo(m * wall_w + wall_w / 2, n * wall_h + wall_h / 10);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 10, n * wall_h + wall_h - wall_h / 4);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 5, n * wall_h + wall_h - wall_h / 4);
+					ctx.lineTo(m * wall_w + wall_w / 2, n * wall_h + wall_h / 2.5);
+					ctx.lineTo(m * wall_w + wall_w / 5, n * wall_h + wall_h - wall_h / 4);
+					ctx.lineTo(m * wall_w + wall_w / 10, n * wall_h + wall_h - wall_h / 4);
+					ctx.closePath();
+					ctx.fill();
+				} else if (maze[n][m] == D) {
+					const grad1 = ctx.createLinearGradient(0, Math.floor(n * wall_h), 0, Math.ceil(n * wall_h + wall_h));
+					grad1.addColorStop(0, theme['speedy'][0]);
+					grad1.addColorStop(1, theme['speedy'][1]);
+					ctx.fillStyle = grad1;
+					ctx.fillRect(Math.ceil(m * wall_w), Math.ceil(n * wall_h), Math.floor(wall_w), Math.floor(wall_h));
+					ctx.fillStyle = theme['speedy_arr'];
+					ctx.beginPath();
+					ctx.moveTo(m * wall_w + wall_w / 2, n * wall_h + wall_h - wall_h / 10);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 10, n * wall_h + wall_h / 4);
+					ctx.lineTo(m * wall_w + wall_w - wall_w / 5, n * wall_h + wall_h / 4);
+					ctx.lineTo(m * wall_w + wall_w / 2, n * wall_h + wall_h - wall_h / 2.5);
+					ctx.lineTo(m * wall_w + wall_w / 5, n * wall_h + wall_h / 4);
+					ctx.lineTo(m * wall_w + wall_w / 10, n * wall_h + wall_h / 4);
+					ctx.closePath();
+					ctx.fill();
 				}
 			}
 		}
 	},
 	drawCircle: () => {
-		ctx.fillStyle = '#00c';
+		ctx.fillStyle = theme['circlee'];
+		ctx.strokeStyle = theme['circlee_border'];
+		ctx.lineWidth = 1.3;
 		ctx.beginPath();
 		ctx.arc(circlee.x, circlee.y, circlee.size, 0, 2 * Math.PI);
 		ctx.fill();
+		ctx.stroke();
 	},
 	drawSquare: () => {
-		ctx.fillStyle = '#e90';
+		ctx.fillStyle = theme['squaree'];
 		ctx.fillRect(squaree.x - squaree.size, squaree.y - squaree.size, squaree.size * 2, squaree.size * 2);
+		ctx.strokeStyle = theme['squaree_border'];
+		ctx.lineWidth = 1.3;
+		ctx.strokeRect(squaree.x - squaree.size, squaree.y - squaree.size, squaree.size * 2, squaree.size * 2);
 	},
 	findSqPath: () => {
 		let ci_grid_pos = gridConvert([circlee.x, circlee.y], fromG=false);
 		let sq_grid_pos = gridConvert([squaree.x, squaree.y], fromG=false);
 		if (ci_grid_pos[0] == gridConvert([circlee.last_x, circlee.last_y], fromG=false)[0]
 			&& ci_grid_pos[1] == gridConvert([circlee.last_x, circlee.last_y], fromG=false)[1]
-			&& sq_grid_pos == gridConvert([squaree.last_x, squaree.last_y], fromG=false)) {
-			console.log('baaaaaaaaaaaaaaaaaaaaa');
+			&& sq_grid_pos[0] == gridConvert([squaree.last_x, squaree.last_y], fromG=false)[0]
+			&& sq_grid_pos[1] == gridConvert([squaree.last_x, squaree.last_y], fromG=false)[1]) {
+			// console.log('baaaaaaaaaaaaaaaaaaaaa');
 			return;
 		}
-		// console.log('ci: ' + ci_grid_pos);
+		// console.log('ci: ' + ci+ wall_h / 2), 0, Math.floor(m * wall_w + wall_w / 2), Math.floor(n * wall_h + wall_h / 2_grid_pos);
 		// console.log('ci_l: ' + gridConvert([circlee.last_x, circlee.last_y], fromG=false));
 		// console.log(ci_grid_pos == gridConvert([circlee.last_x, circlee.last_y], fromG=false));
 		// console.log('ci_l_raw: ' + [circlee.last_x, circlee.last_y]);
@@ -186,22 +362,24 @@ const game = {
 			}
 		}
 		let layer = 0;
-		// let d_opt = JSON.parse(JSON.stringify(dirs));
-		// for (n in d_opt) {
-			
-		// }
 		if (to_an[0][0] == sq_grid_pos[0] && to_an[0][1] == sq_grid_pos[1]) {
-			// console.log([0, 0]);
 			squaree.direction = [0, 0];
-			// console.log('layer: ' + layer);
 			return;
 		}
 		layer++;
 		do {
 			let cur_node = to_an[0];
+			if (cur_node[0] == tp2_grid_pos[0][0] && cur_node[1] == tp2_grid_pos[0][1]) {
+				cur_node = JSON.parse(JSON.stringify(tp2_grid_pos[1]));
+			} else if (cur_node[0] == tp2_grid_pos[1][0] && cur_node[1] == tp2_grid_pos[1][1]) {
+				cur_node = JSON.parse(JSON.stringify(tp2_grid_pos[0]));
+			}
 			// console.log('cn: ' + cur_node);
 			for (d in dirs) {
-				if (maze[cur_node[1] + dirs[d][1]] == undefined || visited[cur_node[1] + dirs[d][1]][cur_node[0] + dirs[d][0]] || maze[cur_node[1] + dirs[d][1]][cur_node[0] + dirs[d][0]] != 0) {
+				if (maze[cur_node[1] + dirs[d][1]] == undefined
+					|| visited[cur_node[1] + dirs[d][1]][cur_node[0] + dirs[d][0]]
+					|| maze[cur_node[1] + dirs[d][1]][cur_node[0] + dirs[d][0]] == 1
+					|| maze[cur_node[1] + dirs[d][1]][cur_node[0] + dirs[d][0]] == undefined) {
 					// console.log(maze[cur_node[1] + dirs[d][1]] == undefined);
 					// if (maze[cur_node[1] + dirs[d][1]] != undefined) {
 						// console.log(
@@ -284,20 +462,23 @@ const game = {
 };
 
 let maze = [
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+[1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, S, 0, 1, 0, 1, 0, 0],
+[0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+[0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0],
+[0, 0, 0, 1, 0, 2, 2, 2, 0, 0, 0, 0, 0, C, 0, 0, 0, 3, 0, 0],
+[0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[0, 0, R, R, R, R, R, R, R, R, R, R, R, 0, 0, 0, 0, 1, 0, 0],
+[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+[0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 3, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, C, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0],
+[0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, L, 0, 0, 0, 0, 0, 0],
+[S, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
+
+// let maze = [[5]];
+
 // let maze = [
 // [0, 0, 0, 0, 0, 0, 0],
 // [0, 0, 0, 0, 0, 0, 0],
@@ -308,6 +489,13 @@ let maze = [
 
 let rows, cols, wall_w, wall_h;
 game.updateMaze();
+
+let tp_grid_pos = [[7, 10], [13, 5]];
+let tp_pos = [gridConvert(tp_grid_pos[0], center=true), gridConvert(tp_grid_pos[1], center=true)];
+
+let tp2_grid_pos = [[0, 12], [13, 2]];
+let tp2_pos = [gridConvert(tp2_grid_pos[0], center=true), gridConvert(tp2_grid_pos[1], center=true)];
+
 let spawnPos = gridConvert([0, 0], center=true);
 circlee.x = spawnPos[0];
 circlee.y = spawnPos[1];
@@ -321,11 +509,80 @@ function collCheck(ch_x, ch_y, obj, setPos=true) {
 	let b_up, b_dn, b_l, b_r;
 	let grid_x = (obj.x - obj.x % (canv_w / cols)) / (canv_w / cols);
 	let grid_y = (obj.y - obj.y % (canv_h / rows)) / (canv_h / rows);
-	
+
+	if (maze[grid_y][grid_x] == 2) {
+		obj.speed = obj.base_speed / 2;
+	} else if (maze[grid_y][grid_x] == L) {
+		if (obj.direction[0] == -1) {
+			obj.speed = obj.base_speed * 3;
+		} else if (obj.direction[0] == 1) {
+			obj.speed = obj.base_speed / 3;
+		} else {
+			obj.speed = obj.base_speed;
+		}
+	} else if (maze[grid_y][grid_x] == R) {
+		if (obj.direction[0] == 1) {
+			obj.speed = obj.base_speed * 3;
+		} else if (obj.direction[0] == -1) {
+			obj.speed = obj.base_speed / 3;
+		} else {
+			obj.speed = obj.base_speed;
+		}
+	} else if (maze[grid_y][grid_x] == U) {
+		if (obj.direction[1] == -1) {
+			obj.speed = obj.base_speed * 3;
+		} else if (obj.direction[1] == 1) {
+			obj.speed = obj.base_speed / 3;
+		} else {
+			obj.speed = obj.base_speed;
+		}
+	} else if (maze[grid_y][grid_x] == D) {
+		if (obj.direction[1] == 1) {
+			obj.speed = obj.base_speed * 3;
+		} else if (obj.direction[1] == -1) {
+			obj.speed = obj.base_speed / 3;
+		} else {
+			obj.speed = obj.base_speed;
+		}
+	} else {
+		obj.speed = obj.base_speed;
+	}
+
+	if (maze[grid_y][grid_x] == C && obj == circlee) {
+		if (!obj.do_not_tp) {
+			if (grid_x == tp_grid_pos[0][0] && grid_y == tp_grid_pos[0][1]) {
+				obj.x = tp_pos[1][0];
+				obj.y = tp_pos[1][1];
+			} else {
+				obj.x = tp_pos[0][0];
+				obj.y = tp_pos[0][1];
+			}
+			obj.direction = [0, 0];
+			obj.do_not_tp = true;
+		}
+	} else if (obj == circlee) {
+		obj.do_not_tp = false;
+	}
+	if (maze[grid_y][grid_x] == S && obj == squaree) {
+		if (!obj.do_not_tp) {
+			if (grid_x == tp2_grid_pos[0][0] && grid_y == tp2_grid_pos[0][1]) {
+				obj.x = tp2_pos[1][0];
+				obj.y = tp2_pos[1][1];
+			} else {
+				obj.x = tp2_pos[0][0];
+				obj.y = tp2_pos[0][1];
+			}
+			obj.direction = [0, 0];
+			obj.do_not_tp = true;
+		}
+	} else if (obj == squaree) {
+		obj.do_not_tp = false;
+	}
+
 	if (grid_x == 0) {
 		b_l = 0;
 	} else {
-		if (maze[grid_y][grid_x - 1]) {
+		if (maze[grid_y][grid_x - 1] == 1) {
 			b_l = (grid_x) * wall_w;
 		} else {
 			b_l = 0;
@@ -334,7 +591,7 @@ function collCheck(ch_x, ch_y, obj, setPos=true) {
 	if (grid_y == 0) {
 		b_up = 0;
 	} else {
-		if (maze[grid_y - 1][grid_x]) {
+		if (maze[grid_y - 1][grid_x] == 1) {
 			b_up = (grid_y) * wall_h;
 		} else {
 			b_up = 0;
@@ -343,7 +600,7 @@ function collCheck(ch_x, ch_y, obj, setPos=true) {
 	if (grid_x == cols - 1) {
 		b_r = cols * wall_w;
 	} else {
-		if (maze[grid_y][grid_x + 1]) {
+		if (maze[grid_y][grid_x + 1] == 1) {
 			b_r = (grid_x + 1) * wall_w;
 		} else {
 			b_r = cols * wall_w;
@@ -352,7 +609,7 @@ function collCheck(ch_x, ch_y, obj, setPos=true) {
 	if (grid_y == rows - 1) {
 		b_dn = rows * wall_h;
 	} else {
-		if (maze[grid_y + 1][grid_x]) {
+		if (maze[grid_y + 1][grid_x] == 1) {
 			b_dn = (grid_y + 1) * wall_h;
 		} else {
 			b_dn = rows * wall_h;
@@ -398,6 +655,7 @@ function gameLoop() {
 	// }
 	squaree.x += ch_x2;
 	squaree.y += ch_y2;
+	collCheck(ch_x2, ch_y2, squaree, setPos = true);
 	if (!collCheck(ch_x, ch_y, circlee)) {
 		circlee.last_x = circlee.x;
 		circlee.last_y = circlee.y;
@@ -408,7 +666,7 @@ function gameLoop() {
 		game.frame();
 	}
 }
-game.randomizeMaze_2();
+// game.randomizeMaze_2();
 squaree.y = canv_h - wall_h / 2;
 for (let m = cols - 1; m >= 0; m--) {
 	if (maze[rows - 1][m] == 0) {
